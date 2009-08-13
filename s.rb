2b0a40d511a845a740558d9604afc7b8e033c366
Jim Weirich <jim.weirich@gmail.com>
2009-08-13 14:19:50 -0400

    updated the ruby solution s.rb

--- s.rb
+++ s.rb
@@ -1,23 +1,20 @@
-#!/usr/bin/env ruby
+#!/usr/bin/env ruby19
 
 def r
-  (0..80).each do |i|
+  (0..80).each { |i|
     next if A[i] > 0
-    t = {}
-    bad = (0..80).map { |j|
+    ((1..9).to_a - (0..80).map { |j|
       (j/9 == i/9 ||
         j%9 == i%9 ||
         j/27 == i/27 && j%9/3 == i%9/3) ? A[j] : 0
-    }.uniq
-    guesses = ((1..9).to_a - bad)
-    guesses.each do |g|
+    }).each { |g|
       A[i] = g
       r
       A[i] = 0
-    end
+    }
     return
-  end
-  puts A.join('').scan(/.{9}/)
+  }
+  puts A.join
   exit
 end
 A = gets.strip.split(//).map {|n| n.to_i }
